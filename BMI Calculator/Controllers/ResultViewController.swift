e7580ad95b0af0de2d4d1ba6aaf00b58f831f0e7
IvanAronov <greasy-phenols-0e@icloud.com>
2022-03-20 17:30:38 +0500

    Complete App

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Ivan Aronov on 12.02.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+}
